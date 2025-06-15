b5d18423339f22946322a79e23d8b70dcfb6fd3f
zachlucas1 <zachtlucas@gmail.com>
2025-06-14 20:50:27 -0600

    updates to bring in breed

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-//gets a request
-function httpGet(theUrl)
-{
+// gets a request
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
 
 // gets a random image from this API
-function getRandomImage()
-{
-  // get the json from a server that has 20,000 images
-  var json = httpGet('https://dog.ceo/api/breeds/image/random');
-  console.log(json);
+function getRandomImage() {
+    // get the json from the API
+    var json = httpGet('https://dog.ceo/api/breeds/image/random');
+    console.log(json);
 
-  // puts it all into an array
-  var array = JSON.parse(json);
-  console.log(array);
-  
-  // get the image url from the array
-  var url = array.message;
-  console.log(url);
-  
-  // get the image 
-  var image = document.getElementById('dogImage1');
+    // parse the JSON response
+    var array = JSON.parse(json);
+    console.log(array);
 
-  
-  // set the src of the image object and prints it
-  image.src = url;
+    // get the image URL
+    var url = array.message;
+    console.log(url);
+
+    // update the image
+    var image = document.getElementById('dogImage1');
+    image.src = url;
+
+    // extract the breed from the URL
+    var breedMatch = url.match(/breeds\/([^\/]+)\//);
+    var breedName = "Unknown Breed";
+
+    if (breedMatch && breedMatch[1]) {
+        breedName = breedMatch[1]
+            .replace('-', ' ')                     // Replace hyphens with spaces
+            .replace(/\b\w/g, c => c.toUpperCase()); // Capitalize each word
+    }
+
+    // update the breed name in the DOM
+    var breedElement = document.getElementById('breedName');
+    if (breedElement) {
+        breedElement.innerText = `Breed: ${breedName}`;
+    }
 }
